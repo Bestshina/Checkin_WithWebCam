cac539cdc056cbe761e01e4cd368c1f61126932c
Skynet <nathapoj@gmail.com>
2022-07-13 13:03:15 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Checkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.Capture_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic1
+            // 
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Location = new System.Drawing.Point(12, 12);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(514, 433);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(582, 12);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(510, 433);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartBtn.Location = new System.Drawing.Point(12, 474);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(170, 49);
+            this.StartBtn.TabIndex = 2;
+            this.StartBtn.Text = "Strat";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // Capture_btn
+            // 
+            this.Capture_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Capture_btn.Location = new System.Drawing.Point(209, 474);
+            this.Capture_btn.Name = "Capture_btn";
+            this.Capture_btn.Size = new System.Drawing.Size(136, 49);
+            this.Capture_btn.TabIndex = 3;
+            this.Capture_btn.Text = "Capture";
+            this.Capture_btn.UseVisualStyleBackColor = true;
+            this.Capture_btn.Click += new System.EventHandler(this.Capture_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 586);
+            this.Controls.Add(this.Capture_btn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button Capture_btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace Checkin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        VideoCaptureDevice Vdodevice;
+        FilterInfoCollection FiltersInfo;
+
+        void StartCamera()
+        {
+            try
+            {
+                FiltersInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                Vdodevice = new VideoCaptureDevice(FiltersInfo[0].MonikerString);
+                Vdodevice.NewFrame += new NewFrameEventHandler(Camera_On);
+                Vdodevice.Start();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void Camera_On(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Vdodevice.Stop();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void Capture_btn_Click(object sender, EventArgs e)
+        {
+            pic2.Image = pic1.Image;
+            string filename = @"C:\Image\" + System.DateTime.Now.ToString("dd_MM_yyyy_hh_mm_ss_tt") + ".jpg";
+
+            var bitmap = new Bitmap(pic2.Width, pic2.Height);
+            pic2.DrawToBitmap(bitmap, pic2.ClientRectangle);
+            System.Drawing.Imaging.ImageFormat imageFormat = null;
+            imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+            bitmap.Save(filename, imageFormat);
+        }
+    }
+}
